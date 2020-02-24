63b337a36d6d0416747310f9ee4c2951972eae77
rtkaaho <adam.aho@arcticwolf.com>
2020-02-24 15:29:08 -0500

    removing old icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,15 +1,5 @@
-import CheckSolid from './CheckSolid';
-import CircleNotch from './CircleNotch';
 import Loading from './Loading';
-import MinusSolid from './MinusSolid';
-import PlusSolid from './PlusSolid';
-import TimesSolid from './TimesSolid';
 
 export const Icon = {
-  CheckSolid,
-  CircleNotch,
   Loading,
-  MinusSolid,
-  PlusSolid,
-  TimesSolid,
 };
