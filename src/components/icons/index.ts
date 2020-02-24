867ec0067db904c9c5d41685b0815a5b90de5804
rtkaaho <adam.aho@arcticwolf.com>
2020-02-24 16:03:11 -0500

    fixing tests

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,15 @@
+import Check from './Check';
 import Loading from './Loading';
+import Minus from './Minus';
+import Plus from './Plus';
+import Times from './Times';
 
 export const Icon = {
+  Check,
   Loading,
+  Minus,
+  Plus,
+  Times,
 };
+
+export { IconProps } from './types';
