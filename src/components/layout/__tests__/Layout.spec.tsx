6bf87db0bcee50fec43623f6f9a97d910e365265
aaho <adam.aho@arcticwolf.com>
2020-03-04 13:44:33 -0500

    adding sider capability to layout

--- src/components/layout/__tests__/Layout.spec.tsx
+++ src/components/layout/__tests__/Layout.spec.tsx
@@ -30,4 +30,28 @@ describe('Layout', () => {
     expect(wrapper.exists('#content')).toBe(true);
     expect(wrapper.exists('#footer')).toBe(true);
   });
+
+  it('renders layout with sider', () => {
+    const wrapper = shallow(
+      <Layout>
+        <Layout.Sider>
+          <span id="sider"></span>
+        </Layout.Sider>
+        <Layout.Header>
+          <span id="header">Header</span>
+        </Layout.Header>
+        <Layout.Content>
+          <span id="content">Content</span>
+        </Layout.Content>
+        <Layout.Footer>
+          <span id="footer">Footer</span>
+        </Layout.Footer>
+      </Layout>
+    );
+
+    expect(wrapper.exists('#header')).toBe(true);
+    expect(wrapper.exists('#content')).toBe(true);
+    expect(wrapper.exists('#footer')).toBe(true);
+    expect(wrapper.exists('#sider')).toBe(true);
+  });
 });
