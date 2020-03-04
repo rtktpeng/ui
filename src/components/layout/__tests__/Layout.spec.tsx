861cb29fbed6d2595eb3e2e636aa780b5e6338fd
aaho <adam.aho@arcticwolf.com>
2020-03-04 13:53:24 -0500

    removing unneeded layout test

--- src/components/layout/__tests__/Layout.spec.tsx
+++ src/components/layout/__tests__/Layout.spec.tsx
@@ -5,12 +5,6 @@ import { shallow } from 'enzyme';
 import { Layout } from '../Layout';
 
 describe('Layout', () => {
-  it('renders Layout', () => {
-    const wrapper = shallow(<Layout>children</Layout>);
-
-    expect(wrapper.exists('Layout__Container')).toBe(true);
-  });
-
   it('renders full layout', () => {
     const wrapper = shallow(
       <Layout>
