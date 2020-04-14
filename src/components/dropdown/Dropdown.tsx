851c994f98ff229c599c4d9cecff5d28c9f6a552
aaho <adam.aho@arcticwolf.com>
2020-04-14 12:42:01 -0400

    working dropdown

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -2,20 +2,73 @@ import * as React from 'react';
 
 import styled from 'styled-components';
 
+import { Floater } from '../floater/Floater';
+
 export interface DropdownProps {
   /** className of the dropdown component */
   className?: string;
+
+  /** trigger to show the dropdown item  */
+  trigger?: 'hover' | 'click';
+
+  /** content to show in the dropdown */
+  overlay?: React.ReactNode;
 }
 
-const Container = styled.div``;
+const Container = styled.span`
+  cursor: pointer;
+  padding-bottom: 20px;
+  box-sizing: border-box;
+`;
+
+const OverlayContainer = styled.div`
+  background: white;
+
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  border-radius: 6px;
+
+  min-width: 200px;
+`;
 
 export const Dropdown: React.FunctionComponent<DropdownProps> = ({
   children,
   className,
+  overlay,
+  trigger,
 }) => {
+  const [open, setOpen] = React.useState(false);
+  const fooRef = React.useRef(null);
+
+  const handleMouseEnter = React.useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const handleMouseLeave = React.useCallback(() => {
+    setOpen(false);
+  }, []);
+
   return (
-    <Container className={`${className} rtk-dropdown `}>{children}</Container>
+    <Container
+      ref={fooRef}
+      className={`${className} rtk-dropdown `}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {children}
+      <Floater
+        anchorElement={fooRef.current}
+        position={['bl', 'tl']}
+        open={open && fooRef !== null}
+      >
+        <OverlayContainer>{overlay}</OverlayContainer>
+      </Floater>
+    </Container>
   );
+};
+
+Dropdown.defaultProps = {
+  trigger: 'hover',
 };
 
 Dropdown.displayName = 'Dropdown';
--- src/components/floater/Floater.tsx
+++ src/components/floater/Floater.tsx
@@ -149,7 +149,7 @@ Floater.defaultProps = {
     initial: { opacity: 0 },
     animate: { opacity: 1 },
     exit: { opacity: 0 },
-    transition: { duration: 0.3 },
+    transition: { duration: 0.1 },
   },
   disableFloater: false,
   container: undefined,
